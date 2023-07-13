95970ee02ef5165f1c3b59e591ffef7b08cdc0a7
0793rebecca <rebeccafrempongmensah@gmail.com>
2023-07-13 16:57:47 +0000

    program that imports the variable a

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -2,6 +2,6 @@
 
 if __name__ == "__main__":
     """Print a program that imports the variable a from the file variable_load_5.py and prints its value."""
-    from variable_load_5 import a
+    from variable_load_5 import v
 
-    print(a)
+    print(v)
