81a293ad74738d5aa17685c28bc2d25aa2444f50
0793rebecca <rebeccafrempongmensah@gmail.com>
2023-07-17 18:10:57 +0000

    a function that deletes the item at a specific position in a list

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    final = []
-    for num in my_list:
-        final.append(num % 2 == 0)
-    return final
+    """Find all multiples of 2 in a list."""
+    multiples = []
+    for i in range(len(my_list)):
+        if my_list[i] % 2 == 0:
+            multiples.append(True)
+        else:
+            multiples.append(False)
+
+    return (multiples)
