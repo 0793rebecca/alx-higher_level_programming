f3a254ee062dfadaf48cb318dd021593176979b6
0793rebecca <rebeccafrempongmensah@gmail.com>
2023-07-31 15:48:25 -0700

    a function that executes a function safely

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
 
-def magic_calculation(a, b):
-    result = 0
 
-    for i in range(1, 3):
-        try:
-            if i > a:
-                raise Exception('Too far')
-            result += a ** b / i
-        except Exception as ex:
-            result = a + b
-            break
+def safe_function(fct, *args):
+    """Executes a function safely.
 
-    return result
+    Args:
+        fct: The function to execute.
+        args: Arguments for fct.
+
+    Returns:
+        If an error occurs - None.
+        Otherwise - the result of the call to fct.
+    """
+    try:
+        result = fct(*args)
+        return (result)
+    except:
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return (None)
